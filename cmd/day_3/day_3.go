6604cee9a284a933ac14004e684deda37ed77769
Alisher Ganiev <alisher_ganiev@apple.com>
2020-11-23 21:37:31 -0800

    Day 3

---
+++ cmd/day_3/day_3.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var n uint
+
+	fmt.Scan(&n)
+
+	if n%2 != 0 {
+		fmt.Println("Weird")
+	} else if n%2 == 0 && (n >= 2 && n <= 5) {
+		fmt.Println("Not Weird")
+	} else if n%2 == 0 && (n >= 6 && n <= 20) {
+		fmt.Println("Weird")
+	} else {
+		fmt.Println("Not Weird")
+	}
+}
